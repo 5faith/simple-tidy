05bd4f052240f490909b63053efacf7573715438
5faith <1475307570@qq.com>
2025-02-20 15:23:27 +0800

    :construction_worker:  env finish

---
+++ PLAN.md
@@ -0,0 +1,18 @@
+[toc]
+
+> 选择[ruoyi-cloud](https://doc.ruoyi.vip/ruoyi-cloud/)为基础架构快速开发, 深入了解Cloud相关组建
+
+# 1. 需求部分
+- 
+- [系统整体架构图](https://note.youdao.com/s/QOnbw7dZ)
+![架构图](assert/jiagoutu.png)
+
+- [概要设计](https://note.youdao.com/s/cwBD7PZk)
+- [详细设计](https://note.youdao.com/s/Sga2cU5m)
+
+# 2. 系统环境搭建
+
+- mysql
+- redis
+- nacos
+- nginx
--- deploy/env/mysql/init/init.sql
+++ deploy/env/mysql/init/init.sql
@@ -1,2 +1,15 @@
--- 检查并创建数据库
+-- 创建simple_tidy数据库
+CREATE DATABASE IF NOT EXISTS simple_tidy;
+
+-- 创建nacos_config数据库
 CREATE DATABASE IF NOT EXISTS nacos_config;
+
+-- 创建tidy用户并授予simple_tidy数据库的所有权限
+CREATE USER 'tidy'@'%' IDENTIFIED BY 'tidy@55555';
+GRANT ALL PRIVILEGES ON simple_tidy.* TO 'tidy'@'%';
+
+-- 创建nacos用户并授予nacos_config数据库的所有权限
+CREATE USER 'nacos'@'%' IDENTIFIED BY 'nacos@55555';
+GRANT ALL PRIVILEGES ON nacos_config.* TO 'nacos'@'%';
+
+FLUSH PRIVILEGES;
